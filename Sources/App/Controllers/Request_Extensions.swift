7e1ef904c2265af83dddcf8c1a2b280c9cea1cf1
David van Enckevort <david@allthingsdigital.nl>
2019-08-01 13:50:42 +0200

    Remove dead code

--- Sources/App/Controllers/Request_Extensions.swift
+++ Sources/App/Controllers/Request_Extensions.swift
@@ -10,12 +10,6 @@ import Vapor
 import os
 
 extension Request {
-    var url: URL? {
-        let baseURL = Environment.get("baseURL") ?? "http://localhost:8080/"
-        print(baseURL)
-        return URL(string: baseURL)
-    }
-
     var filter: FilterParameters? {
         return try? query.decode(FilterParameters.self)
     }
